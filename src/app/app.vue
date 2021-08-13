ee6166bfe5e037d94d08f03fd3425795b56aec18
xiaoxuan <xiaoxuan@qq.com>
2021-08-13 08:41:34 +0800

    文本框（Text）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,12 +1,15 @@
 <template>
   <h3>{{ name }}</h3>
+  <input type="text" v-model="content">
+  <span>> Content: {{content}}</span>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      name: 'NINGHAO',
+      name: '宁皓网',
+      content: '天地玄黄'
     };
   },
 };
