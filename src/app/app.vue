63cc667e11dd68aa484d985743f800f79710a40c
xiaoxuan <xiaoxuan@qq.com>
2021-08-13 11:01:49 +0800

    监听子组件的事件

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,15 +1,15 @@
 <template>
   <h3>{{ name }}</h3>
-  <AppButton text="确定"></AppButton>
-  <AppButton />
-  <app-button></app-button>
-  <app-button/>
-  <app-demo-button />
+  <AppButton 
+    text="确定"
+    @tap="onTapAppButton"
+    @init="onInitAppButton"
+  ></AppButton>
 </template>
 
 <script>
 import AppButton from './components/app-button.vue';
-import AppDemoButton from './components/app-demo-button.vue';
+
 export default {
   data() {
     return {
@@ -18,7 +18,17 @@ export default {
     };
   },
 
-  components: {AppButton, AppDemoButton}
+  methods: {
+    onTapAppButton(text) {
+      console.log('on tap app button', text);
+    },
+
+    onInitAppButton(text) {
+      console.log('on init app button', text);
+    },
+  },
+
+  components: {AppButton},
 };
 </script>
 
