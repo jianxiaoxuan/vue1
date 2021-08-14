451ec36e73334226f2bd90dbd67a50fa25c0e66c
xiaoxuan <xiaoxuan@qq.com>
2021-08-14 17:49:33 +0800

    组合式接口：提供组件方法

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,22 +1,21 @@
 <template>
-  <h3 @click="name = 'NINGHAO'">{{ name }}</h3>
-  <div @click="username = 'xiaoxuan'">{{username}}</div>
+  <h3 @click="changeName">{{ name }}</h3>
 </template>
 
 <script>
-import { reactive, ref, toRefs } from 'vue';
+import { ref } from 'vue';
 
 export default {
   setup() {
     const name = ref('宁皓网');
 
-    const user = reactive({
-      username: '小炫',
-    });
+    const changeName = () => {
+      name.value = 'NINGHAO';
+    };
 
     return {
       name,
-      ...toRefs(user),
+      changeName,
     };
   },
 };
