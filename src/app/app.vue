f3e0794706c7cdb695cfa04618703eba946192d9
xiaoxuan <xiaoxuan@qq.com>
2021-08-14 17:09:35 +0800

    状态的过渡

--- src/app/app.vue
+++ src/app/app.vue
@@ -6,17 +6,10 @@
         <div class="card-subtitle">Transition & Animation</div>
       </div>
       <div class="card__content">
-        <transition-group name="custom"
-          leave-active-class="animate__animated animate__tada"
-        >
-        <div class="emoji" v-for="emoji in emojiList" :key="emoji">
-          {{ emoji }}
-        </div>
-        </transition-group>
+        <div class="emoji">{{animatedNumber}}</div>
       </div>
       <div class="card__action">
-        <button @click="shuffle" :class="{ active: isActive }">请按这里</button>
-        <button @click="pop">请按这里</button>
+        <button @click="number = number + 10" :class="{ active: isActive }">请按这里</button>
       </div>
     </div>
     <div class="status">
@@ -27,6 +20,7 @@
 
 <script>
 import _ from 'lodash';
+import gsap from 'gsap';
 
 export default {
   data() {
@@ -34,7 +28,21 @@ export default {
       name: '宁皓网',
       isActive: true,
       emojiList: ['🌲', '🌳', '🌴'],
+      number: 0,
+      tweenedNumber: 0,
     };
+  },
+
+  computed: {
+    animatedNumber() {
+      return this.tweenedNumber.toFixed(0);
+    },
+  },
+
+  watch: {
+    number(newValue) {
+      gsap.to(this.$data, {duration: 0.5, tweenedNumber: newValue});
+    },
   },
 
   methods: {
