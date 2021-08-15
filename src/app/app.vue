7ce29531348266ddc42dbf0b92c4fb26b3207edd
xiaoxuan <xiaoxuan@qq.com>
2021-08-15 14:01:22 +0800

    安装与使用HTTP客户端请求服务端接口（axios）

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,25 +1,27 @@
 <template>
   <h3>{{ name }}</h3>
-  <div class="menu">
-    <div
-      :class="['menu-item',{ active: currentItem === index }]"
-      @click="currentItem = index"
-      v-for="(item, index) in menuItems"
-      :key="index"
-    >
-      {{item}}
-    </div>
+  <div v-for="post in posts" :key="post.id">
+    {{post.title}} - 
+    <small>{{post.user.name}}</small>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      name: 'NINGHAO',
-      menuItems: ['首页', '热门', '发布'],
-      currentItem: 0,
+      name: '宁皓网',
+      posts: [],
     };
+  },
+
+  created() {
+    axios.get('http://localhost:3000/posts').then(response => {
+      console.log(response);
+      this.posts = response.data;
+    });
   },
 };
 </script>
