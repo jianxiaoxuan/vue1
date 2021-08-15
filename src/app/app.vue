5d75eaa4cd5ea535dc97d2176ebb3e6495100bca
xiaoxuan <xiaoxuan@qq.com>
2021-08-15 14:06:07 +0800

    处理在Vue应用里使用axios发送请求时发生的错误

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <h3>{{ name }}</h3>
+  <div>{{errorMessage}}</div>
   <div v-for="post in posts" :key="post.id">
     {{post.title}} - 
     <small>{{post.user.name}}</small>
@@ -14,13 +15,21 @@ export default {
     return {
       name: '宁皓网',
       posts: [],
+      errorMessage: ''
     };
   },
 
   created() {
-    axios.get('http://localhost:3000/posts').then(response => {
+    axios.get('http://localhost:3000/posts1')
+    .then(response => {
       console.log(response);
       this.posts = response.data;
+    })
+    .catch(error => {
+      console.log(error.message);
+      console.log(error.response);
+
+      this.errorMessage = error.message;
     });
   },
 };
